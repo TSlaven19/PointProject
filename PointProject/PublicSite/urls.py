833c92e238966608d6f6c15ebaea6ad0e32ff1f2
Tobin S <dukeslaven@gmail.com>
2019-02-13 15:26:33 -0500

    Added routing and page for special

--- PointProject/PublicSite/urls.py
+++ PointProject/PublicSite/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('score', views.score, name='score'),
+    path('special', views.special, name='special'),
+    path('score_special', views.score_special, name='score_special'),
 ]
--- PointProject/PublicSite/views.py
+++ PointProject/PublicSite/views.py
@@ -23,14 +23,15 @@ def index(request):
         return context
 
     context = total_points()
-    if (len(event_list) > 4):
-        event_list = event_list[len(event_list)-5:]
+    if len(event_list) > 4:
+        event_list = event_list[len(event_list) - 5 :]
         context["event_list"] = event_list[::-1]
     else:
         context["event_list"] = event_list[::-1]
     context["point_list"] = point_list
-    
+
     return render(request, "PublicSite/index.html", context)
+
 
 def score(request):
     e = Event()
@@ -41,4 +42,48 @@ def score(request):
     e.event_scoree = req["Scoree"]
     e.save()
 
-    return redirect('/')
+    return redirect("/")
+
+
+def special(request):
+    event_list = Event.objects.all()
+    point_list = Point.objects.all()
+
+    def total_points():
+        anna_count = 0
+        tobin_count = 0
+        total = 0
+        for e in event_list:
+            total += e.point.point_value
+            if e.event_scoree == "AMK":
+                anna_count += e.point.point_value
+            elif e.event_scoree == "TDS":
+                tobin_count += e.point.point_value
+            else:
+                anna_count += e.point.point_value
+                tobin_count += e.point.point_value
+        context = {"total": total, "anna": anna_count, "tobin": tobin_count}
+        return context
+
+    context = total_points()
+    if len(event_list) > 4:
+        event_list = event_list[len(event_list) - 5 :]
+        context["event_list"] = event_list[::-1]
+    else:
+        context["event_list"] = event_list[::-1]
+    context["point_list"] = point_list
+
+    return render(request, "PublicSite/special.html", context)
+
+
+def score_special(request):
+    req = request.POST
+    p = Point(point_name = req["Point"], point_value = req["Value"], point_active = False)
+    p.save()
+    e = Event()
+    e.point = p
+    e.event_scoree = req["Scoree"]
+    e.save()
+
+    return redirect("/")
+
