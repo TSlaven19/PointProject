51d01ea71cc3d74fcf7a4e4889614c6c4cfc0596
Duke Slaven <dslaven@wreathsacrossamerica.org>
2019-03-16 21:36:38 +0000

    CSS and JS compression

--- PointProject/PointProject/settings.py
+++ PointProject/PointProject/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "compressor",
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,23 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'compressor.finders.CompressorFinder'
+]
+
+# Cache
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': 'unix:/tmp/memcached.sock',
+    }
+}
+
+# Compress
+
+COMPRESS_ROOT = '/comp/'
+COMPRESS_ENABLED = True
